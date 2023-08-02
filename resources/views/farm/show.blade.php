8fc7821187d230e3ac9298c2ec032706ab9d4bd7
Arcaz22 <chandraarcychan@gmail.com>
2023-08-02 16:40:59 +0700

    fixing show and edit

--- resources/views/farm/show.blade.php
+++ resources/views/farm/show.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <h1>Detail Kandang</h1>
-    <p><strong>Nama Kandang:</strong> {{ $kandang->nama_kandang }}</p>
-    <p><strong>Kepala Kandang:</strong> {{ $kandang->kepala_kandang }}</p>
-    <p><strong>Status Kandang:</strong> {{ $kandang->status_kandang }}</p>
+    <p><strong>Nama Kandang:</strong> {{ $farm->nama_kandang }}</p>
+    <p><strong>Kepala Kandang:</strong> {{ $farm->kepala_kandang }}</p>
+    <p><strong>Status Kandang:</strong> {{ $farm->status_kandang }}</p>
     <a href="{{ route('farm.index') }}" class="btn btn-secondary">Kembali</a>
 </div>
 @endsection
