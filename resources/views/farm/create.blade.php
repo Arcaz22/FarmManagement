367b110da6a8cc7d095876ca75f968c1f4c48f54
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-03 00:44:57 +0700

    build index and create data

--- resources/views/farm/create.blade.php
+++ resources/views/farm/create.blade.php
@@ -8,33 +8,42 @@
                 <h3 class="modal-title" id="createData">Tambah Kandang</h3>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <form action="{{ route('farm.store') }}" method="post">
+            <form action="{{ route('farm.store') }}" method="post">
+                <div class="modal-body">
                     @csrf
-                    <div class="form-group">
+                    <div class="form-group mt-1">
                         <label for="nama_kandang">Nama Kandang</label>
                         <input type="text" class="form-control" id="nama_kandang" name="nama_kandang" required>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mt-1">
                         <label for="kepala_kandang">Kepala Kandang</label>
                         <input type="text" class="form-control" id="kepala_kandang" name="kepala_kandang" required>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mt-1">
                         <label for="status_kandang">Status Kandang</label>
-                        <input type="text" class="form-control" id="status_kandang" name="status_kandang" required>
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Silakan pilih status kandang</option>
-                            @foreach ($collection as $item)
-                                <option value="">One</option>
-                            @endforeach
+                        <select class="form-select" id="status_kandang" name="status_kandang" required>
+                            <option selected disabled>Silakan pilih status kandang</option>
+                            <option value="panen">Panen</option>
+                            <option value="chick in">Chick In</option>
+                            <option value="persiapan kandang">Persiapan Kandang</option>
+                            <option value="cuci kandang">Cuci Kandang</option>
+                            <option value="kosong">Kosong</option>
                         </select>
                     </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Simpan</button>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
+                    <div class="form-group mt-1">
+                        <label for="kapasitas_kandang">Kapasitas Kandang</label>
+                        <input type="text" class="form-control" id="kapasitas_kandang" name="kapasitas_kandang" required>
+                    </div>
+                    <div class="form-group mt-1">
+                        <label for="lokasi_kandang">Lokasi Kandang</label>
+                        <input type="text" class="form-control" id="lokasi_kandang" name="lokasi_kandang" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/farm/show.blade.php
+++ resources/views/farm/show.blade.php
@@ -1,11 +1,34 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="container">
-    <h1>Detail Kandang</h1>
-    <p><strong>Nama Kandang:</strong> {{ $farm->nama_kandang }}</p>
-    <p><strong>Kepala Kandang:</strong> {{ $farm->kepala_kandang }}</p>
-    <p><strong>Status Kandang:</strong> {{ $farm->status_kandang }}</p>
+    <div class="row align-items-center justify-content-around">
+        <h1 class="col-4">Farm Management</h1>
+        <div class="col">
+            <div class="row justify">
+                <div class="col">
+                    <a href="" class="btn btn-light">Search</a>
+                </div>
+                <div class="col">
+                    <a href="" class="btn btn-light">Filter</a>
+                </div>
+                <div class="col">
+                    {{-- <a href="{{ route('farm.create') }}" class="btn btn-primary" style="border-radius: 30px"><i class="bi bi-plus"></i>Tambah Kandang</a> --}}
+                    @include('farm.create')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <ul class="list-group">
+        <li class="list-group-item"><strong>Nama Kandang:</strong> {{ $farm->nama_kandang }}</li>
+        <li class="list-group-item"><strong>Kepala Kandang:</strong> {{ $farm->kepala_kandang }}</li>
+        <li class="list-group-item"><strong>Status Kandang:</strong> {{ $farm->status_kandang }}</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+    </ul>
+
+    <p></p>
+    <p></p>
+    <p></p>
     <a href="{{ route('farm.index') }}" class="btn btn-secondary">Kembali</a>
-</div>
 @endsection
