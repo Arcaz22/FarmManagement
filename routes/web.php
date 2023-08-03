c956aac853a4528cbf407e0f7434b03f5f5f662a
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-03 08:00:12 +0700

    fixing dashboard - sidebar

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('dashboard', function () {
     return view('dashboard');
 });
-
 
 Route::get('/farm/search', [FarmController::class, 'search'])->name('farm.search');
 Route::get('/farm/filter', [FarmController::class, 'filter'])->name('farm.filter');
